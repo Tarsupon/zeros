6a2df3abf97b64dddf25b985ccea243970c0e03c
Aliaxei Hlushakou <alexeyka981@gmail.com>
2018-09-26 14:16:11 +0300

    norm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let zeroCount = 0;
+  for (var i = 5; number / i >= 1; i = i * 5)
+    zeroCount += Math.floor(number/i);
+  return zeroCount;
 }
